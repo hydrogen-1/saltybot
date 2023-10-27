32c938d979115255f0056dd0687593413c4408d7
Johannes Becker <jbecker@student.tugraz.at>
2023-10-27 18:12:11 +0200

    don't busy wait

--- twitchwsclient.py
+++ twitchwsclient.py
@@ -2,6 +2,7 @@ import websocket
 from dataclasses import dataclass
 from collections.abc import Iterator
 import threading
+from queue import Queue
 
 @dataclass
 class Credentials():
@@ -13,11 +14,11 @@ class TwitchWsClient():
     def __init__(self, credentials: Credentials) -> None:
         self.wsapp = websocket.WebSocketApp("wss://irc-ws.chat.twitch.tv:443", on_message=self.handle_message, on_open=self.handle_connect, on_ping=self.handle_ping)
         self.creds = credentials
-        self.message = ""
+        self.message_queue = Queue()
 
     def handle_message(self, wsapp :websocket.WebSocketApp, msg: str) -> None:
         if(msg.startswith(":waifu4u!")):
-            self.message = msg
+            self.message_queue.put(msg)
         elif(msg == "PING"):
             print("PING")
             wsapp.send("PONG :tmi.twitch.tv")
@@ -35,8 +36,6 @@ class TwitchWsClient():
         self.ws_thread.start()
 
     def messages(self) -> Iterator[str]:
-        last_message = ""
         while(True):
-            if(self.message != last_message):
-                yield(self.message)
-                last_message = self.message
+            message = self.message_queue.get()
+            yield(message)
